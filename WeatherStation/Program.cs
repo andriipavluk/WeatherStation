2681ee5b26c0e25b9c460889faad509876948ea2
PAVLUK <PAVLUK@DESKTOP-B0RH3GD>
2025-12-05 09:23:08 +0100

    Initial implementation

---
+++ WeatherStation/Program.cs
@@ -0,0 +1,9 @@
+﻿using Database;
+
+User user1 = new User("Ja", 999);
+User user2 = new User("Ja", 999);
+Sensor sensor1 = new Sensor();
+
+Console.WriteLine($"Utworzono użytkownika 1: {user1.Name} (ID: {user1.Id})");
+Console.WriteLine($"Utworzono użytkownika 2: {user2.Name} (ID: {user2.Id})");
+Console.WriteLine("Utworzono sensor 1");
