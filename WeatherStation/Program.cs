996e207ee2f2abb93b67cc233161eb977440d6d4
PAVLUK <PAVLUK@DESKTOP-B0RH3GD>
2025-12-10 17:11:33 +0100

    wszystkie zadania

--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Database;
 
-User user1 = new User("Ja", 999);
+User user1 = new User("alkashka", 999);
 User user2 = new User("Ja", 999);
 Sensor sensor1 = new Sensor();
 
 Console.WriteLine($"Utworzono użytkownika 1: {user1.Name} (ID: {user1.Id})");
 Console.WriteLine($"Utworzono użytkownika 2: {user2.Name} (ID: {user2.Id})");
 Console.WriteLine("Utworzono sensor 1");
+bool i = user1.Equals( user2 );
+Console.WriteLine(i);
