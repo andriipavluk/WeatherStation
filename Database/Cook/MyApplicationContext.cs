4415352a1b10f18cd5422549477ee734d4100a21
EwaSladkowa <sladkowayewa126@gmail.com>
2026-01-16 14:57:50 +0100

    dodanie framework sql server

---
+++ Database/Cook/MyApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics.Metrics;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Database.MyContext
+{
+    public class MyApplicationContext : DbContext
+    {
+        public DbSet<Sensor> Sensors { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
+        }
+    }
+}
