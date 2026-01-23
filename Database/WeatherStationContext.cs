50e3d8a80616fa80f5d98f65bbe533287cedc22d
EwaSladkowa <sladkowayewa126@gmail.com>
2026-01-23 15:39:04 +0100

    dodanie nuget 23 stycznia

---
+++ Database/WeatherStationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class WeatherStationContext : DbContext
+    {
+        public DbSet<Sensor> Sensors { get; set; }
+        public DbSet<User> Users { get; set; }
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("<CONNECTION STRING>");
+        }
+    }
+}
--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -1,11 +1,14 @@
 ﻿using Database;
-
-User user1 = new User("alkashka", 999);
-User user2 = new User("Ja", 999);
-Sensor sensor1 = new Sensor();
-
-Console.WriteLine($"Utworzono użytkownika 1: {user1.Name} (ID: {user1.Id})");
-Console.WriteLine($"Utworzono użytkownika 2: {user2.Name} (ID: {user2.Id})");
-Console.WriteLine("Utworzono sensor 1");
-bool i = user1.Equals( user2 );
-Console.WriteLine(i);
+namespace WeatherStation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var myContext = new WeatherStationContext())
+            {
+                myContext.Database.EnsureCreated();
+            }
+        }
+    }
+}
