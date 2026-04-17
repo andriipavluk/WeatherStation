e07f78283c9a0384a5b427a40b5dd800b679233e
William <dzdz@local>
2026-04-17 15:51:46 +0200

    dodanie unit tests

---
+++ UnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UnitTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -7,13 +7,14 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        using (var myContext = new WeatherStationContext())
+        using (var myContext = new MyApplycationContext())
         {
             myContext.Database.EnsureCreated();
-            //IUserRepository user = new UI;
-            var user = new UserRepository(context);
-            var tempo = context.user.GetUser(1);
-            Console.WriteLine(tempo.Name);
+
+            IUserRepository userRepository = new UserRepository(myContext);
+
+            var randomUser = Random.Shared.Next(1000) + 1;
+            Console.WriteLine(randomUser?.Name ?? "User doesn't exist");
         }
     }
 }
